0df52dbcbe34835862e4db246c2ae7d175b6ee5e
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2018-07-08 22:30:31 +0200

    Added React Intl

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,32 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
-
+import { injectIntl } from 'react-intl'
 import Header from './header'
-import './layout.css'
 
-const Layout = ({ children, data }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+const Layout = ({ children, data, intl }) => (
+  <div>
+    <Helmet
+      title={intl.formatMessage({ id: 'title' })}
+      meta={[
+        { name: 'description', content: intl.formatMessage({ id: 'welcome' }) },
+        {
+          name: 'keywords',
+          content: 'gatsby, i18n, react-intl, multi language, localization',
+        },
+      ]}
+    />
+    <Header siteTitle={intl.formatMessage({ id: 'title' })} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children}
+    </div>
+  </div>
 )
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default injectIntl(Layout)
---
+++ src/layouts/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './index.css'
+
+const DefaultLayout = ({ children }) => <div>{children()}</div>
+
+export default DefaultLayout
