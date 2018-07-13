6e9fc7613de5ccfc41c8a60b301a1fdd6f4c75ee
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2018-07-13 21:36:33 +0200

    Updated story

--- src/i18n/locales/hu.js
+++ src/i18n/locales/hu.js
@@ -1,11 +1,80 @@
 module.exports = {
-  'site.title': 'Hófehérke',
+  'site.title': 'Hófehérke és a hét törpe',
+  'site.subtitle': 'Meseelemzés felnőtteknek',
   'site.description': ' ',
   'site.keywords': ' ',
+
   'character.queen': 'Királynő',
   'character.snowwhite': 'Hófehérke',
   'character.stepmother': 'Mostoha',
   'character.king': 'Király',
   'character.prince': 'Herceg',
   'character.huntsman': 'Vadász',
+
+  'story.introduction.title': 'Tudtad?',
+  'story.introduction.desc': 'Hogy a hófehérke mese valójában az egyik legősibb kuktúrális emlékünk, és amelynek fő témája a párkapcsolat, a szexualitás és az élet misztikus körforgása?',
+
+  'story.storyteller.title': 'A mesemondás',
+  'story.storyteller.desc': 'A mesét az idősebb generáció – szülők, nagyszülők – mondják el a legfiatalabb generációnak, a kisgyerekeknek. A gyermekek érdekes módon nagyon nyitottak ezekre a történetekre, saját maguk kérik, akár újra és újra ugyanazt a történetet, mint egyfajta szellemi táplálékot, a madárfiókához hasonlóan nyaggatva a szülőket. Élvezik a történeteket, legyenek azok bármennyire borzasztóan félelmetesek vagy bizarrak.',
+
+  'story.storyteller.legacy.title': 'Hagyaték',
+  'story.storyteller.legacy.desc': 'A felnőttek tapasztaltak, az idősek még inkább. Túl vannak szerelmen, háborún, életen és halálon, szinte már mindent megtapasztaltak, amit csak lehet… Felnőttként, ha visszagondolunk, pár dologtól szívesen megkíméltük volna magunkat, az elszalasztott lehetőségekkel pedig szívesen élnénk – utólag már könnyen okos az ember. Szívesen elmondanánk a kicsiknek, hogy ezt így, azt meg amúgy… De hogyan?',
+
+  'story.storyteller.commonLanguage.title': 'A közös nyelv',
+  'story.storyteller.commonLanguage.desc': 'Igen nehéz helyzet előtt áll az aki mindent el szeretne mondani, mert egy estébe nem fér bele 60 év. És hiába tanácsolná el az unokát a devizahitelektől, mert mire a kicsi felnő, újabb intézmények teszik majd próbára egy megváltozott világban “trükkök százaival”. A felmenői hibáját pedig egyébként is megtapasztalja, azt már nem fogja elkövetni, fog majd mást. Hogy adhatna az ember tanácsot valami olyanban, amivel maga sem boldogult, vagy nem is találkozott?',
+
+  'story.storyteller.philosophersStone.title': 'A bölcsek köve',
+  'story.storyteller.philosophersStone.desc': 'A mese évezredek tanulságait összegzi, képes átívelni korszakokat, képes konzerválni a veszendő információkat. Ráadásul a felnőttek világát egy olyan virágnyelven tárgyalja, amit egy gyermek kezébe is oda lehet adni. A gyermeknek ez a forma tetszik, és úy játszik vele, mint egy labdával, vagy babával, nem is sejtve a “veszélyes” összetevőket. Mert ebben a kis játékban bizony maró, irritatív anyagok vannak, a felnőtt világ kiszámíthatatlan vegyszerei. A mese különleges tulajdonsága a “gyerekzár”, a “védőkupak”, amit gyermek nem képes kinyitni, valljuk be, mi felnőttek pedig már nem is próbáljuk. Pedig most éppen erre fogunk kísérletet tenni. Kiszabadítjuk a szellemet a palackból.',
+
+  'story.storyteller.historyOfStorytelling.title': 'A mesemondás történelme',
+  'story.storyteller.historyOfStorytelling.desc': '3 millió évvel ezelőttől az 1700-as évekig folyamatos, töretlen, élőszóval előadott mesemondás. Személyes emlékeken és megtapasztaláson alapuló történeteket adott elő az idősebb generáció az 1-2-vel fiatalabbnak. 1700 – 1900 Nyomtatásban megjelenet történetek több generációt is át tudtak ívelni. Hogy azért jelentek-e meg a nyomtatott mesék, hogy átmentsék a történeteket, mert az emberek leszoktak a mesemondásról, vagy az emberek azért szoktak le a mesemondásról, mert rendelkezésre állt a kényelmesebb, szórakoztatóbb módszer, nem tudni. De az biztos, hogy a személyre szabott, egyedi történetek elmaradtak, a folyamatosan alakuló, formálódó mesék sztenderdizálódtak, megmerevedtek, és lelkiségük illetve információtartalmuk egyre fogyatkozott, egyre nehezebben hozzáférhetővé vált. 1900 – 2000 Megjelenik a film, a mesék nem pusztán hallás, de szemléletes “mozi” formájában is elérhetőek, ami nagyon lenyűgöző, ám maximálisan konzum, és az eredeti mélyrétegtől mentes, vagy akár azzal ellentétes mondanivaló közlésére is alkalmas. 2000 – A digitáldiktatúra korában lehetővé vált, hogy nem csak elméleti, hanem gyakorlati értelemben is a világ teljes interneten megosztott információja egyidejűleg elérhetővé váljon minden korosztálynak. Ezzel párosul a választás látszólagos szabadsága, így kialakult egy, az elméért vívott információs-média háború. Ez a háború exponenciális ütemben gyűri maga alá globális szinten a teljesen védtelen tömegeket. A fogyasztói társadalom média-ipar-mágnásai szintetikus mesék tömegeit terhelik a felkészületlen emberekre, véglegesen kivégezve ezzel a mese ősi apját, gyökereit. Innentől a mesék pusztán eszközök, fegyverek azok kezében akiknek kellő anyaki forrás áll rendelkezésére, hogy egyfajta befektetésként, a szellemi és lelki szempontból lárvaállapotban lévő tömegek fejlődését olyan irányba alakítsák, hogy később befektetésük megtérüljön...',
+
+  'story.storyteller.emptySymbols.title': 'Szimbólumok',
+  'story.storyteller.emptySymbols.desc': 'Megváltozott világunkból eltűntek azok a használati tárgyak, melyek a régi mesékben szimbólummokként tűntek fel. Ha értelmezni kívánjuk őket, át kell helyeződnünk a középkor emberének világába, egy csendesebb, nyugodtabb, lassabb, természetközeli világba, amikor a léleknek sokkal több figyelem jutott.',
+
+  'story.storyteller.symbols.title': 'A szimbólumok kiüresedése',
+  'story.storyteller.symbols.desc': 'A mese egy érdekes műfaj… Véresen komoly és egyben lágy. Az élet összes színterének valamennyi küzdelmét sűríti egy pontba, egy kicsi, kedves játékba, ami átadható egy gyermeknek, a nélkül, hogy az kárt okozna a gyemeknek, és a nélkül, hogy a gyermek tönkre tehetné… Az egyetlen mód ami ezt lehetővé teszi, egy egyezményes kódnyelv, a szombólumok világa.',
+
+  'story.textil.historyOfIndustry.title': 'A textilipar fejlődése',
+  'story.textil.historyOfIndustry.desc': 'Szögezzük le, nem biztos, hogy mit is csinál a királynő az ablakban, varr, fon, (köt), de nyilván textilipari kézműves tevékenységet végez, miközben megszúrja az ujját. Ma azt is hihetnénk, varrógéppel dolgozott. Jelenleg műanyag ipari termékekben járunk, és nem ismerjük az eredetét az eleve rongyosra szaggatottan kapható göncöknek. Így nagy hátránnyal indulunk a megértéshez. Nem volt ez mindig így, az elmúlt 80 évben sorsdöntő fordulat zajlott le. A legidősebbek még emlékeznek a len, kender feldolgozására, fonásra, szövésre, hímzésre.',
+
+  'story.textil.clothing.title': 'Ruházkodás',
+  'story.textil.clothing.desc': 'A királynő nem luxust űz, hanem egy olyan tevékenységet, amely pár száz éve még hétköznapi női elfoglaltság volt. A nyáron megtermelt anyagokat ősszel betakarították és télen nekiláttak a feldolgozásnak. Hideg téli napokon a természeti munkák megfogyatkoznak, a királynő ekkor – véleményem szerint – fon. Feltételezésem okai: a következő szimbólumok: fonal, orsó.',
+
+  'story.textil.fates.title': 'Párkák',
+  'story.textil.fates.desc': 'A három ősi boszorka az élet fonalát sodorja. Az egyik a szöszt adja, a másik sodorja, a harmadik a fonalat elvgja Gubanc, tekervény, sodrat, fonal, szövet, textil...  Az ablaknál fonó királynő előrevetíti a jövő történéseit: hófehérke “gubancba” keveredik, az élet messzire “sodorja”. Ebben a kon”textus”ban a királynő egy párka, ábrándokat “sző”. A mostoha “fon”dorlatos, a történet “fonala” “tekervényes”.',
+
+  'story.textil.dna.title': 'DNS',
+  'story.textil.dna.desc': 'Kettős spirál ami az élet kialakításához szükséges információt hordozza. Egyetlen sejtben (megtermékenyült pete) a sejtmagon belül a teljes kifejlett szervezet felépítéséher szükséges teljes információmennyiség benne van. + képes a változó környezetre reagálni. Egy fonat, mely időről időre összehúzódik és kitárul. Összegubancolódik vagy “kiegyenesedik”. Olykor tárolási üzemmódban van, megmerevedik, védekezik, olykor olvasásiban, ilyenkor ellágyul, működésbe lép, kibontja szerkezetét.',
+
+  'story.textil.umbilicalCord.title': 'Köldökzsinór',
+  'story.textil.umbilicalCord.desc': 'A látható biológiai szál, mely fizikailag összeköti anyát gyermekével.  Az anyának is van köldöke, a lánygyermeknek méhe, mutatva a végtelen láncolatot – Matrjoska baba hasonlat. Ha szaporosásbiológiailag vizsgáljuk meg a mese mondanivalóját, ismét érdekes dolgot találunk: A királynő, aki egy nőstény, épp az ablaknál “hímez”. (A hímpor, hímzés díszítő jellege a hím állatok díszességére utal, igy jogos a feltételezés)',
+
+  'story.textil.lifeBlood.title': 'A vércsepp, a vérvonal',
+  'story.textil.lifeBlood.desc': 'Az orsó csak a mesekönyvekben néz ki úgy ahogy. Az ősi orsók, amely időből a történet is származik, nem rokkán voltak, hanem önmagukban használhatóak voltak. Ez egy hosszúkás szerszám, egy 15-20 cm-es “rudacska” egyik végén egy “bumszlival”, ami elhegyesedik. Megszúrni valakit viszont csak a másik végéről lehet… Phallikus szimbólumnak is nevezhetnénk. 1 csepp vér hull az orsó nyomán, és ez az alábbiakra enged következtetni: 1., Nem ért vénát, 2., röviddel az orsó behatolása után elmaradt a vérzése, 3., esetlegesen először szúrta meg orsó, és ez kevés vér elfolyásával járt.',
+
+  'character.queen.desc': 'A nő ragyogó oldala. Az ablak szimbólum mutatja, hogy befogadó. Belső világa nyitott a külvilág felé. A fény beáramlása. Fényből sző. Illuzórikus. Az ábrándjai vezérlik, nem figyel oda mit tesz így megsebesíti magát. Önfeláldozó. Túlságosan is, belepusztul, helyét a sötét oldal veszi át.',
+  'character.snowwhite.desc': 'Naív de tiszta Oktondi: újonc a világban, mit sem sejt a mostoha és a vadász ármánykodásáról. Világnézetileg felkészületlen, sok tanulás vár még rá. Gyönge: Az útjáról nem ő, hanem nagyobb erők és érdekek döntenek. Ő maga csak csapong. Létének beteljesedése a Királynő karakter, ennek eléréséhez meg kell találnia a herceget. De oda a sötét erdőn keresztül vezet az út. Ha elbukik, maga is mostohává válik.',
+  'character.stepmother.desc': 'Női sötét oldal. Fő probléma, hogy gonosz, önző, magával törődő, minden mással “mostoha”. Szép, a legszebb. Ezt idővel elveszíti. Okos, tudományokban jártas (“boszorkányságban”). Ezt a tudását gonosz célokra használja. Tisztán látó, már nem naív, tisztában van az igazsággal (igazmondó tükre van). De ezzel nem néz szembe, (összetöri azt), illúziókba ringatja magát (a vadász átveri) vagy szembemegy a valósággal (hófehérkét mérgezi)  Hatalmi pozícióban van (királynő) Vannak boldog pillanatai, de összességében nem lehet boldognak nevezni.',
+  'character.king.desc': 'Szeretetteljes. Az örök hó, a fény birodalmának királya. Megvilágosodottként tudhatja, mi történik hófehérkével (a világosság szimbólumával) Tudhatja, hogy a fényt épp készül elnyelni a sötétség, mégsem tesz semmit. Nem harcol, mégis győz.',
+  'character.prince.desc': 'Férfi út lárvaállapota: A mese említést sem tesz róla, mintha nem is létezne. Az erdőben bukkan fel, abban az erdőben ahol a vadász hófehérkét helyben hagyta. Felmerülhet a gyanú, hogy mit keresett a herceg az erdőben. Nyilván egy hófehérkét, de akkor mi különbözteti meg a vadásztól?',
+  'character.huntsman.desc': 'A férfi sötét oldala. Szintén “mostoha” Tapasztalt, úttalan utakon jár, eligazodik a sötét erdőben, az alvilágban. Mégis, egy nő zárja végül börtönbe. Ott is marad, a sötétség rabja lesz. Férfias: vadakkal kűzd meg, fegyvere van (phallikus szimbólumként) Sajnos ezúttal nem méltó ellenfelet választ, védtelen gyermeket is majdnem megsebesít, mert nem áll azonnal ellen a gonosz felkérésnek. Amor nyila: szerelemben járatos, mégis úgy tűnik, egyedül van senkinek sem hiányzik se felbukkanása előtt, se utána. Csak egy “disznó” szívét tudja megszerezni. Boldogtalan fantom, kósza kísértet',
+
+  'symbol.paths.title': 'Utak',
+  'symbol.paths.desc': 'Lárvaállapot: hófehérke, herceg. Női út, férfi út – egyedi út, de megfeleltethető. Felisút-leisút – minden állapotból el lehet mozdulni, mindegyiknek vannak jellemzői. A mese kisarkítja a karakterek megítélését, de egyben rávilágít, nincs abszolút jó és rossz, ez nézőpont, “viszony” kérdése. A viszonylatok határozzák meg a lehetleges elmozdulási irányokat. Horizontális, vertikális utak, személyiségfejlődés',
+
+  'symbol.forest.title': 'Helyszín – Erdő',
+  'symbol.forest.desc': '...',
+
+  'symbol.home.title': 'Helyszín - otthon',
+  'symbol.home.desc': 'Fehérvár – Feketevár',
+
+  'symbol.mirror.title': 'Tükör',
+  'symbol.mirror.desc': '…',
+
+  'symbol.jail.title': 'A börtön',
+  'symbol.jail.desc': 'A mostoha börtönbe veti a szolgálatot megtagadó vadászt. Mind a vadász, mind a királyné mellékvágányra van állítva… Véleményem szerint a vadász-királyné kapcsolat azonos a király-mostoha kapcsolattal, csak a nemek és reakciók különböznek. Nem a vadász van börtönben, pusztán a vadász-börtön szimbólum számít. Avagy, ha a “vadász” van a királynő szívébe börtönözve, abba belehal, vagy valami hasonló…',
+
+  'symbol.mine.title': 'Bánya',
+  'symbol.mine.desc': 'A sötét erdő mélyén egy sötét föld alatti üreg. Nem kerül bemutatásra. Feltehetően kincset rejt. Megéri kockáztatni, megéri mélyre ásni...',
 }
