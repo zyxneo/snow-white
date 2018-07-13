494449ffa91a7010c84b8e10e9094dbf5ead9d3a
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2018-07-13 17:44:44 +0200

    Update StoryGraph - bezier curve, external data

--- src/components/StoryGraph/StoryGraph.jsx
+++ src/components/StoryGraph/StoryGraph.jsx
@@ -1,68 +1,121 @@
 import React from 'react'
-import { injectIntl, FormattedMessage } from 'react-intl'
 
 import './StoryGraph.scss'
 
+const AvatarWidth = 100
+const AvatarHeight = 100
+// const scale = percent => `scale(${percent} ${percent})`
+
+const translateCenterString = (cx, cy, scalePercent) => {
+  const tx = (cx / scalePercent) - (AvatarWidth / 2)
+  const ty = (cy / scalePercent) - (AvatarHeight / 2)
+  return `translate(${tx} ${ty})`
+}
+
+const scaleString = (scalePercentX, scalePercentY) => {
+  return `scale(${scalePercentX} ${scalePercentY})`
+}
+
+const scaleAvatar = (cx, cy, scalePercent) => `${scaleString(scalePercent, scalePercent)} ${translateCenterString(cx, cy, scalePercent)}`
+
+const drawBezier = (x1, y1, x2, y2, vertical, strength = 0.5) => {
+  if (vertical) {
+    return `M ${x1},${y1} C ${x1},${y1 + (y2 - y1) * strength}, ${x2},${y2 - (y2 - y1) * strength}, ${x2},${y2}`
+  }
+  return `M ${x1},${y1} C ${x1 + (x2 - x1) * strength},${y1}, ${x2 - (x2 - x1) * strength},${y2}, ${x2},${y2}`
+}
+
+
 const Avatar = (props) => {
+  const { character } = props
+
   const {
     id,
-    color,
-    title,
-    x = 0,
-    y = 0,
-    width = 100,
-    height = 100
-  } = props
+    gender,
+    name,
+    imageX = 0,
+    imageY = 0,
+    imageWidth = 100,
+    imageHeight = 100,
+    cx,
+    cy,
+    scale,
+  } = character
 
+  let color
+  switch (gender) {
+    case 'female':
+      color = 'pink'
+      break
+
+    case 'male':
+      color = 'skyblue'
+      break
+
+    default:
+      color = 'gray'
+      break
+  }
   return (
-    <symbol id={id}>
+    <g id={id} transform={scaleAvatar(cx, cy, scale)}>
       <circle id="avatarCircle" cx="50" cy="50" r="32" fill="none" />
       <circle cx="50" cy="50" r="30" fill={color} />
       <circle cx="50" cy="50" r="25" fill="none" stroke="black" strokeWidth=".5" />
       <g clipPath="url(#circleMask)">
-        <image x={x} y={y} href={`/avatars/${id}.jpg`} width={width} height={height} />
+        <image x={imageX} y={imageY} href={`/avatars/${id}.jpg`} width={imageWidth} height={imageHeight} />
       </g>
       <text textAnchor="middle" className="avatar-title">
         <textPath href="#avatarCircle" startOffset="75%">
-          {title}
+          {name}
         </textPath>
       </text>
-    </symbol>
+    </g>
   )
 }
-const Scale = 0.25
-const scale = percent => `scale(${percent} ${percent})`
-const setXY = (x, y) => `scale(${Scale} ${Scale}) translate(${x / Scale} ${y / Scale})`
 
-const StoryGraph = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" xmlns-xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" width="800" height="800">
+const StoryGraph = (props) => {
+  const { graphData } = props
 
-    <defs>
-      <clipPath id="circleMask">
-        <circle cx="50" cy="50" r="25" />
-      </clipPath>
-    </defs>
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns-xlink="http://www.w3.org/1999/xlink" viewBox="0 0 120 120" width="800" height="800">
 
-    <Avatar id="queen" color="pink" title={<FormattedMessage id="character.queen" defaultMessage="Queen" tagName="tspan" />} />
-    <Avatar id="snowwhite" color="pink" title={<FormattedMessage id="character.snowwhite" defaultMessage="Snow White" tagName="tspan" />} />
-    <Avatar id="witch" color="pink" title={<FormattedMessage id="character.stepmother" defaultMessage="Stepmother" tagName="tspan" />} x="-4" y="20" width="120" height="120" />
-    <Avatar id="huntsman" color="skyblue" title={<FormattedMessage id="character.huntsman" defaultMessage="Huntsman" tagName="tspan" />} width="50" height="100" x="25" y="10" />
-    <Avatar id="prince" color="skyblue" title={<FormattedMessage id="character.prince" defaultMessage="Prince" tagName="tspan" />} width="50" height="100" x="25" y="10" />
-    <Avatar id="king" color="skyblue" title={<FormattedMessage id="character.king" defaultMessage="King" tagName="tspan" />} width="50" height="100" x="25" y="10" />
+      <defs>
+        <clipPath id="circleMask">
+          <circle cx="50" cy="50" r="25" />
+        </clipPath>
+      </defs>
 
-    <g>
-      <rect x="0" y="0" width="100" height="100" fill="none" stroke="black" strokeWidth="1" />
-      <use href="#queen" x="0" y="0" transform={setXY(0, 50)} />
-      <use href="#snowwhite" x="0" y="0" transform="scale(0.25 0.25) translate(100 10)" />
-      <use href="#witch" x="0" y="0" transform="scale(0.25 0.25) translate(10 100)" />
-      <use href="#huntsman" x="0" y="0" transform="scale(0.25 0.25) translate(100 100)" />
-      <use href="#prince" x="0" y="0" transform="scale(0.25 0.25) translate(100 200)" />
-      <use href="#king" x="0" y="0" transform="scale(0.25 0.25) translate(10 200)" />
-    </g>
-    <rect x="5" y="5" width="30" height="30" fill="none" stroke="black" strokeWidth="1" />
-    <rect x="65" y="5" width="30" height="30" fill="none" stroke="black" strokeWidth="1" />
-    <rect x="35" y="35" width="30" height="30" fill="none" stroke="black" strokeWidth="1" />
-  </svg>
-)
 
-export default injectIntl(StoryGraph)
+      <rect x="0" y="0" width="120" height="120" fill="none" stroke="black" strokeWidth="1" />
+
+      <symbol id="scene">
+        {
+          graphData.characters.map(character => <Avatar key={character.id} character={character} />)
+        }
+
+        <path d="M 30,37.5 a 25,25,0,0,1,22.5,22.5" stroke="red" strokeWidth="1" fill="none" />
+
+        <path id="huntsman-snowwhite" d="M 90,82.5 c -10,0, -20,-15, -30,-15" stroke="red" strokeWidth="1" fill="none" />
+        <path id="witch-snowwhite" d="M 75,97.5 c -10,0, -5,-30, -15,-30" stroke="red" strokeWidth="1" fill="none" />
+      </symbol>
+
+      <use href="#scene" x="0" y="0" />
+      {/*
+      <use href="#scene" x="60" y="0" />
+      <use href="#scene" x="-60" y="0" />
+      <use href="#scene" x="0" y="90" />
+      <use href="#scene" x="60" y="90" />
+      <use href="#scene" x="0" y="-90" />
+      <use href="#scene" x="-60" y="-90" />
+
+      <circle cx="24" cy="32" r="3" fill="orange" />
+      <circle cx="91" cy="54" r="3" fill="orange" />
+      <path d={drawBezier(24, 32, 91, 54)} stroke="red" strokeWidth="1" fill="none" />
+      <path d={drawBezier(24, 32, 91, 54, true)} stroke="red" strokeWidth="1" fill="none" />
+      */}
+
+      <path d={drawBezier(graphData.characters[0].cx, graphData.characters[0].cy, graphData.characters[1].cx, graphData.characters[1].cy, true)} stroke="red" strokeWidth="1" fill="none" />
+    </svg>
+  )
+}
+export default StoryGraph
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,9 +4,87 @@ import { withIntl, Link } from '../i18n'
 import Layout from '../components/layout'
 import StoryGraph from '../components/StoryGraph'
 
+const graphData = {
+  characters: [
+    {
+      id: 'queen',
+      name: <FormattedMessage id="character.queen" defaultMessage="Queen" tagName="tspan" />,
+      gender: 'female',
+      imageX: 0,
+      imageY: 0,
+      imageWidth: 100,
+      imageHeight: 100,
+      cx: 22.5,
+      cy: 37.5,
+      scale: 0.25,
+    },
+    {
+      id: 'snowwhite',
+      name: <FormattedMessage id="character.snowwhite" defaultMessage="Snow White" tagName="tspan" />,
+      gender: 'female',
+      imageX: 0,
+      imageY: 0,
+      imageWidth: 100,
+      imageHeight: 100,
+      cx: 52.5,
+      cy: 67.5,
+      scale: 0.25,
+    },
+    {
+      id: 'witch',
+      name: <FormattedMessage id="character.stepmother" defaultMessage="Stepmother" tagName="tspan" />,
+      gender: 'female',
+      imageX: -4,
+      imageY: 20,
+      imageWidth: 120,
+      imageHeight: 120,
+      cx: 82.5,
+      cy: 97.5,
+      scale: 0.25,
+    },
+    {
+      id: 'king',
+      name: <FormattedMessage id="character.king" defaultMessage="King" tagName="tspan" />,
+      gender: 'male',
+      imageX: 0,
+      imageY: 0,
+      imageWidth: 100,
+      imageHeight: 100,
+      cx: 37.5,
+      cy: 22.5,
+      scale: 0.25,
+    },
+    {
+      id: 'prince',
+      name: <FormattedMessage id="character.prince" defaultMessage="Prince" tagName="tspan" />,
+      gender: 'male',
+      imageX: 25,
+      imageY: 10,
+      imageWidth: 50,
+      imageHeight: 100,
+      cx: 67.5,
+      cy: 52.5,
+      scale: 0.25,
+    },
+    {
+      id: 'huntsman',
+      name: <FormattedMessage id="character.huntsman" defaultMessage="Huntsman" tagName="tspan" />,
+      gender: 'male',
+      imageX: 25,
+      imageY: 10,
+      imageWidth: 50,
+      imageHeight: 100,
+      cx: 97.5,
+      cy: 82.5,
+      scale: 0.25,
+    },
+  ],
+}
+
+
 const SecondPage = () => (
   <Layout>
-    <StoryGraph />
+    <StoryGraph graphData={graphData} />
   </Layout>
 )
 
