b03e6535f65297b7e4e9156382aed0b18377ce1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-13 15:17:13 +0000

    Deploy zyxneo/snow-white to github.com/zyxneo/snow-white.git:gh-pages

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-  ],
-}
--- src/components/StoryGraph/index.js
+++
@@ -1,7 +0,0 @@
-import StoryGraph from './StoryGraph'
-import graphData from './graphData'
-
-export {
-  StoryGraph,
-  graphData,
-}
