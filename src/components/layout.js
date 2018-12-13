f28a36780bd85aeea666833d769b1e38772eb7c3
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2018-12-13 21:20:51 +0100

    Fix BrowserRouter

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,46 @@
 // @flow
 
 import React from 'react'
+import type { Node } from 'react'
+// $FlowIgnore
 import Helmet from 'react-helmet'
-import { injectIntl } from 'react-intl'
-import '../../node_modules/sanitize.css/sanitize.css'
+
+// $FlowIgnore
+import 'sanitize.css/sanitize.css'
 // $FlowIgnore
 import 'semantic-ui-css/semantic.min.css'
+
+// $FlowIgnore
 import './layout.scss'
 
-const Layout = ({
-  children, data, intl, bodyClass,
-}) => (
-    <div>
-      <Helmet
-        title={intl.formatMessage({ id: 'site.title' })}
-        meta={[
-          {
-            name: 'description',
-            content: intl.formatMessage({ id: 'site.description' }),
-          },
-          {
-            name: 'keywords',
-            content: intl.formatMessage({ id: 'site.keywords' }),
-          },
-        ]}
-        bodyAttributes={{
-          class: bodyClass,
-        }}
-      />
-      <link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative|Fondamento|Pirata+One|Snowburst+One&amp;subset=latin-ext" rel="stylesheet" />
-      <div className="layout">
-        {children}
-      </div>
+type Props = {
+  children: Node,
+  bodyClass: string,
+}
+
+const Layout = ({ children, bodyClass }: Props): Node => (
+  <div>
+    <Helmet
+      title="Snow White"
+      meta={[
+        {
+          name: 'description',
+          content: '...',
+        },
+        {
+          name: 'keywords',
+          content: '...',
+        },
+      ]}
+      bodyAttributes={{
+        class: bodyClass,
+      }}
+    />
+    <link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative|Fondamento|Pirata+One|Snowburst+One&amp;subset=latin-ext" rel="stylesheet" />
+    <div className="layout">
+      {children}
     </div>
+  </div>
 )
 
-export default injectIntl(Layout)
+export default Layout
